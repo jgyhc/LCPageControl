057742a7d58409eba7dc711c5cb014a0b21007f6
jgyhc <jgyhc@foxmail.com>
2020-06-08 15:55:17 +0800

    init

--- Example/LCPageControl/LCViewController.m
+++ Example/LCPageControl/LCViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "LCViewController.h"
+#import "LCPageControl.h"
 
-@interface LCViewController ()
-
+@interface LCViewController ()<LCPageControlDelegate>
+@property (weak, nonatomic) IBOutlet LCPageControl *pageControl;
+@property (weak, nonatomic) IBOutlet LCPageControl *customPageControl;
+@property (nonatomic, assign) NSInteger currentPage;
+@property (nonatomic, assign) NSInteger customCurrentPage;
 @end
 
 @implementation LCViewController
@@ -17,13 +21,50 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _customPageControl.delegate = self;
+    _pageControl.numberOfPages = 10;
+    _customPageControl.numberOfPages = 10;
+
+    _pageControl.currentPage = _currentPage;
+    _customPageControl.currentPage = _customCurrentPage;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)previousPageEvent:(id)sender {
+    if (_currentPage > 0) {
+        _currentPage --;
+        _customCurrentPage --;
+        _pageControl.currentPage = _currentPage;
+        _customPageControl.currentPage = _customCurrentPage;
+    }
+
+}
+
+- (IBAction)nextPageEvent:(id)sender {
+    if (_currentPage < 10) {
+        _currentPage ++;
+        _customCurrentPage ++;
+        _pageControl.currentPage = _currentPage;
+        _customPageControl.currentPage = _customCurrentPage;
+    }
+}
+
+
+- (UIControl *)customPageItemWithPageControl:(LCPageControl *)pageControl index:(NSInteger)index {
+    return ({
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [button setTitle:@"0" forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor blueColor] forState:UIControlStateSelected];
+        button;
+    });
+}
+
+- (CGSize)customPageItemSizeWithPageControl:(LCPageControl *)pageControl index:(NSInteger)index {
+    return CGSizeZero;
+}
+
+- (CGFloat)customPageItemSpaceingWithPageControl:(LCPageControl *)pageControl {
+    return 3;
 }
 
 @end
--- LCPageControl.podspec
+++ LCPageControl.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'LCPageControl'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of LCPageControl.'
+  s.summary          = '自定义的页码控制器 LCPageControl.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -39,4 +39,5 @@ TODO: Add long description of the pod here.
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
+  s.dependency 'Masonry'
 end
